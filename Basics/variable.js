261b11b1e201260b8e3cc3182db08f4fe815e697
Saurav Upadhyay <sauravupadhayay31@gmail.com>
2026-08-12 15:53:13 +0000

    All javascript

---
+++ Basics/variable.js
@@ -0,0 +1,20 @@
+const account_Id = 1234
+let account_Email = "sauravup18@gmail.com"
+accountCity = "Ahmedabad"
+let accountstate; // If we don't know the state yet, we can leave it undefined.
+
+account_Email = "virat@gmail.com"
+accountpassword = "123456789"
+accountCity = "Bengaluru"
+
+console.log(accountCity);
+
+console.table([
+    account_Id,
+    accountstate,
+    account_Email,
+    accountCity,
+    accountpassword
+])
+
+
