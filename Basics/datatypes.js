272463d235e4f71575e00428183234c4a665c33a
Saurav Upadhyay <sauravupadhayay31@gmail.com>
2026-08-12 16:27:30 +0000

    All javascript

---
+++ Basics/datatypes.js
@@ -0,0 +1,31 @@
+
+// Data Types in JavaScript
+// JavaScript has data types similar to other programming languages like Python.
+
+// String
+let Name = "Saurav";
+
+// Number
+let age = 18;
+
+// Boolean
+let IsLoggedIn = true;
+
+// Null
+let data = null;
+
+
+// Checking the data type using typeof
+
+console.log(typeof "Saurav");   // string
+console.log(typeof 18);         // number
+console.log(typeof true);       // boolean
+console.log(typeof undefined);  // undefined
+
+// Note:
+// typeof null returns "object".
+// This is a historical behavior/quirk of JavaScript.
+// Although null is a primitive data type, typeof null gives "object".
+
+console.log(typeof data);       // object , Null means Empty
+
